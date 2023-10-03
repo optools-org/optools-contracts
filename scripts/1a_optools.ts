2053d08c679472fa438c82997787192fc74888d7
un1que <74994059+dekrypt-si@users.noreply.github.com>
2023-10-04 00:00:05 +0200

    Update tsconfig and scripts

--- scripts/1a_optools.ts
+++ scripts/1a_optools.ts
@@ -1,4 +1,5 @@
 import { ethers } from "hardhat";
+import { OpToolsLockerV1 } from "../typechain-types";
 
 const LOCKER_ADDRESS = "0xF32E20dE2ec3e7630dDe035012789a82aEa3e600";
 
@@ -6,12 +7,11 @@ async function main() {
   const Locker = await ethers.getContractFactory('OpToolsLockerV1');
   //const locker = await Locker.deploy();
   //await locker.deployed();
+  //console.log('Locker deployed at ' + locker.address);
 
   const locker = Locker.attach(
     LOCKER_ADDRESS
-  );
-
-  console.log('Locker deployed at ' + locker.address);
+  ) as OpToolsLockerV1
 
   const enableDepositingTx = await locker.setDepositsEnabled(true);
   await enableDepositingTx.wait();
