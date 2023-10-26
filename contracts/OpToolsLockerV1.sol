a4d2e7a627b9652e14310e13f2cbd1fa0e4b8db0
un1que <74994059+dekrypt-si@users.noreply.github.com>
2023-10-26 14:12:57 +0200

    Update OpToolsLockerV1.sol

--- contracts/OpToolsLockerV1.sol
+++ contracts/OpToolsLockerV1.sol
@@ -90,7 +90,17 @@ contract OpToolsLockerV1 is AccessControl, ReentrancyGuard {
 
     /** PUBLIC FUNCTIONS */
 
-    function lockToken(address owner, address token, uint amount, uint timestamp) external payable nonReentrant mustBeEnabled {
+    function lockToken(
+        address owner, 
+        address token, 
+        uint amount, 
+        uint timestamp
+    ) 
+        external 
+        payable 
+        nonReentrant 
+        mustBeEnabled 
+    {
         require (amount > 0, "Amount must be larger than zero");
         require (IERC20(token).balanceOf(_msgSender()) >= amount, "Your token balance is too low");
         require (timestamp >= block.timestamp.add(24 hours), "Unlock timestamp must be at least 24 hours in the future");
@@ -100,7 +110,7 @@ contract OpToolsLockerV1 is AccessControl, ReentrancyGuard {
         userLockCount[owner] = userLockCount[owner].add(1);
 
         uint balanceBefore = IERC20(token).balanceOf(address(this));
-        IERC20(token).transferFrom(_msgSender(), address(this), amount);
+        IERC20(token).safeTransferFrom(_msgSender(), address(this), amount);
         uint balanceAfter = IERC20(token).balanceOf(address(this));
         uint deltaBalance = balanceAfter.sub(balanceBefore);
         Address.sendValue(fund, address(this).balance);
@@ -111,19 +121,36 @@ contract OpToolsLockerV1 is AccessControl, ReentrancyGuard {
         currentLockId++;
     }
 
-    function extendLock(uint lockId, uint timestamp) external mustBeEnabled mustBeOwner(lockId) mustBeLocked(lockId) {
+    function extendLock(
+        uint lockId, 
+        uint timestamp
+    ) 
+        external 
+        mustBeEnabled 
+        mustBeOwner(lockId) 
+        mustBeLocked(lockId) 
+    {
         require (timestamp > locks[lockId].unlockTimestamp, "New unlock timestamp must be further in the future than previous one");
         locks[lockId].unlockTimestamp = timestamp;
         emit LockExtended(lockId, timestamp);
     }
 
-    function increaseLock(uint lockId, uint amount) external nonReentrant mustBeEnabled mustBeOwner(lockId) mustBeLocked(lockId) {
+    function increaseLock(
+        uint lockId, 
+        uint amount
+    ) 
+        external 
+        nonReentrant 
+        mustBeEnabled 
+        mustBeOwner(lockId)
+        mustBeLocked(lockId) 
+    {
         require (amount > 0, "Amount must be larger than zero");
         address token = locks[lockId].token;
         require (IERC20(token).balanceOf(_msgSender()) >= amount, "Your token balance is too low");
 
         uint balanceBefore = IERC20(token).balanceOf(address(this));
-        IERC20(token).transferFrom(_msgSender(), address(this), amount);
+        IERC20(token).safeTransferFrom(_msgSender(), address(this), amount);
         uint balanceAfter = IERC20(token).balanceOf(address(this));
         uint deltaBalance = balanceAfter.sub(balanceBefore);
 
@@ -131,7 +158,19 @@ contract OpToolsLockerV1 is AccessControl, ReentrancyGuard {
         emit LockIncreased(lockId, deltaBalance);
     }
 
-    function splitLock(uint lockId, address owner, uint amountForNewLock, uint timestamp) external payable nonReentrant mustBeEnabled mustBeOwner(lockId) mustBeLocked(lockId) {
+    function splitLock(
+        uint lockId, 
+        address owner, 
+        uint amountForNewLock, 
+        uint timestamp
+    ) 
+        external 
+        payable 
+        nonReentrant 
+        mustBeEnabled 
+        mustBeOwner(lockId) 
+        mustBeLocked(lockId) 
+    {
         require (amountForNewLock > 0, "Amount must be larger than zero");
         require (amountForNewLock <= locks[lockId].amount.sub(locks[lockId].withdrawnAmount), "Amount is bigger than withdrawable amount");
         require (timestamp >= locks[lockId].unlockTimestamp, "Unlock timestamp for split lock can not be lower than existing one");
@@ -151,7 +190,15 @@ contract OpToolsLockerV1 is AccessControl, ReentrancyGuard {
         currentLockId++;
     }
 
-    function transferLockOwnership(uint lockId, address newOwner) external mustBeEnabled mustBeOwner(lockId) mustBeLocked(lockId) {
+    function transferLockOwnership(
+        uint lockId, 
+        address newOwner
+    ) 
+        external 
+        mustBeEnabled 
+        mustBeOwner(lockId) 
+        mustBeLocked(lockId) 
+    {
         _removeLockIdFromUserArray(lockId, _msgSender());
         userLockCount[_msgSender()] = userLockCount[_msgSender()].sub(1);
 
@@ -161,7 +208,51 @@ contract OpToolsLockerV1 is AccessControl, ReentrancyGuard {
         emit LockOwnershipTransferred(lockId, newOwner);
     }
 
-    function unlockToken(uint lockId, uint amount) external nonReentrant mustBeOwner(lockId) mustBeUnlocked(lockId) {
+    function relockToken(
+        uint lockId, 
+        address owner, 
+        uint amount, 
+        uint timestamp
+    ) 
+        external 
+        payable 
+        nonReentrant 
+        mustBeEnabled 
+        mustBeOwner(lockId) 
+        mustBeUnlocked(lockId) 
+    {
+        require (amount > 0, "Amount must be larger than zero");
+        require (amount <= locks[lockId].amount.sub(locks[lockId].withdrawnAmount), "Amount is bigger than eligible amount for relock");
+        require (timestamp >= block.timestamp.add(24 hours), "Unlock timestamp must be at least 24 hours in the future");
+        require (msg.value >= lockFee, "Payable amount must be at least equal to lock fee");
+        address token = locks[lockId].token;
+
+        locks[lockId].withdrawnAmount = locks[lockId].withdrawnAmount.add(amount);
+        if (locks[lockId].withdrawnAmount == locks[lockId].amount) {
+            _removeLockIdFromUserArray(lockId, _msgSender());
+            userLockCount[_msgSender()] = userLockCount[_msgSender()].sub(1);
+        }
+        emit TokenUnlocked(lockId, amount);
+
+        userLockIds[owner].push(currentLockId);
+        userLockCount[owner] = userLockCount[owner].add(1);
+        Address.sendValue(fund, address(this).balance);
+
+        LockInfo memory lockInfo = LockInfo(true, token, owner, amount, timestamp, 0);
+        locks[currentLockId] = lockInfo;
+        emit TokenLocked(currentLockId, token, owner, amount, timestamp);
+        currentLockId++;
+    }
+
+    function unlockToken(
+        uint lockId, 
+        uint amount
+    ) 
+        external 
+        nonReentrant 
+        mustBeOwner(lockId) 
+        mustBeUnlocked(lockId) 
+    {
         require (amount > 0, "Amount must be bigger than zero");
         require (amount <= locks[lockId].amount.sub(locks[lockId].withdrawnAmount), "Amount is bigger than withdrawable amount");
         address token = locks[lockId].token;
@@ -172,13 +263,18 @@ contract OpToolsLockerV1 is AccessControl, ReentrancyGuard {
             userLockCount[_msgSender()] = userLockCount[_msgSender()].sub(1);
         }
 
-        IERC20(token).transfer(locks[lockId].owner, amount);
+        IERC20(token).safeTransfer(locks[lockId].owner, amount);
         emit TokenUnlocked(lockId, amount);
     }
 
     /** INTERNAL FUNCTIONS */
 
-    function _removeLockIdFromUserArray(uint lockId, address owner) internal {
+    function _removeLockIdFromUserArray(
+        uint lockId, 
+        address owner
+    ) 
+        internal 
+    {
         for (uint i; i < userLockIds[owner].length; i++) {
             if (userLockIds[owner][i] == lockId) {
                 userLockIds[owner][i] = userLockIds[owner][userLockIds[owner].length - 1];
@@ -190,28 +286,47 @@ contract OpToolsLockerV1 is AccessControl, ReentrancyGuard {
 
     /** RESTRICTED FUNCTIONS */
 
-    function setDepositsEnabled(bool _depositsEnabled) external onlyRole(MANAGER_ROLE) {
+    function setDepositsEnabled(
+        bool _depositsEnabled
+    ) 
+        external 
+        onlyRole(MANAGER_ROLE) 
+    {
         depositsEnabled = _depositsEnabled;
     }
 
-    function setLockFee(uint _lockFee) external onlyRole(MANAGER_ROLE) {
+    function setLockFee(
+        uint _lockFee
+    ) 
+        external 
+        onlyRole(MANAGER_ROLE) 
+    {
         lockFee = _lockFee;
     }
 
-    function setFund(address _fund) external onlyRole(MANAGER_ROLE) {
+    function setFund(
+        address _fund
+    ) 
+        external 
+        onlyRole(MANAGER_ROLE) 
+    {
         fund = payable(_fund);
     }
 
     /** MODIFIERS */
 
-    modifier mustBeLocked(uint lockId) {
+    modifier mustBeLocked(
+        uint lockId
+    ) {
         require (locks[lockId].exists, "Lock does not exist");
         require (block.timestamp < locks[lockId].unlockTimestamp, "Unlock timestamp already in the past");
 
         _;
     }
 
-    modifier mustBeUnlocked(uint lockId) {
+    modifier mustBeUnlocked(
+        uint lockId
+    ) {
         require (locks[lockId].exists, "Lock does not exist");
         require (block.timestamp >= locks[lockId].unlockTimestamp, "Unlock timestamp is still in the future");
         require (locks[lockId].withdrawnAmount < locks[lockId].amount, "Entire lock amount has already been withdrawn");
@@ -219,14 +334,17 @@ contract OpToolsLockerV1 is AccessControl, ReentrancyGuard {
         _;
     }
 
-    modifier mustBeOwner(uint lockId) {
+    modifier mustBeOwner(
+        uint lockId
+    ) {
         require (locks[lockId].exists, "Lock does not exist");
         require (_msgSender() == locks[lockId].owner, "Caller is not lock owner");
 
         _;
     }
 
-    modifier mustBeEnabled() {
+    modifier mustBeEnabled(
+    ) {
         require (depositsEnabled, "Deposits are currently not enabled");
 
         _;
